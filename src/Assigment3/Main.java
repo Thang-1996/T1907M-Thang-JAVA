6ec1de0fdeea4f5f4c988f0f718bac9b79afb513
ggx1234 <49161576+ggx1234@users.noreply.github.com>
2020-02-26 05:30:01 +0700

    ASS3FIX

--- src/Assigment3/Main.java
+++ src/Assigment3/Main.java
@@ -2,13 +2,16 @@ package Assigment3;
 
 public class Main {
     public static void main(String[] args) {
-        PersonModel<Student> personModel = new PersonModel<Student>();
-        personModel.addStudent(new Student(1,"thang",18));
-        personModel.addStudent(new Student(2,"huyen",18));
-        PersonModel<Employee> personModel1 = new PersonModel<Employee>();
-        personModel1.addEmp(new Employee(1,"thang",20000));
-        personModel1.addEmp(new Employee(2,"huyen",3000000));
-        personModel.showStudent();
-        personModel1.showEmp();
+        PersonModel<Student> personModel = new PersonModel<>();
+        personModel.addPerson(new Student(1,"thang",18));
+        personModel.addPerson(new Student(2,"huyen",18));
+        System.out.println("Student List");
+        personModel.showPerson();
+        PersonModel<Employee> personModel1 = new PersonModel<>();
+        personModel1.addPerson(new Employee(1,"thang",20000));
+        personModel1.addPerson(new Employee(2,"huyen",3000000));
+        System.out.println("Employee List");
+        personModel1.showPerson();
+
     }
 }
--- src/Assigment3/PersonModel.java
+++ src/Assigment3/PersonModel.java
@@ -2,25 +2,14 @@ package Assigment3;
 
 import java.util.ArrayList;
 
-public class PersonModel<S> {
-   ArrayList<Student> person = new ArrayList<>();
-   ArrayList<Employee> person1 = new ArrayList<>();
-   public void addStudent(Student student){
-       person.add(new Student(student.iD,student.name,student.age));
+public class PersonModel<Person> {
+    ArrayList<Person> person = new ArrayList<>();
+   public void addPerson(Person Person){
+       person.add(Person);
    }
-   public void showStudent(){
-       System.out.println("Student List: ");
-       for(Student s : person){
-           System.out.println(s.toString());
+   public void showPerson(){
+       for(Person p : person){
+           System.out.println(p.toString());
        }
    }
-    public void addEmp(Employee employee){
-        person1.add(new Employee(employee.iD,employee.name,employee.salary));
-    }
-    public void showEmp(){
-        System.out.println("Emp List: ");
-        for(Employee e : person1){
-            System.out.println(e.toString());
-        }
-    }
 }
