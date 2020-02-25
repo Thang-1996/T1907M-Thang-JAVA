0318b397d9e2dd3ef0facf246a6e1b196fae5b3d
ggx1234 <49161576+ggx1234@users.noreply.github.com>
2020-02-26 05:31:42 +0700

    ASS3FIX2

--- src/Assigment3/Main.java
+++ src/Assigment3/Main.java
@@ -2,14 +2,14 @@ package Assigment3;
 
 public class Main {
     public static void main(String[] args) {
-        PersonModel<Student> personModel = new PersonModel<>();
+        PersonModel<Student> personModel = new PersonModel<>(); //
         personModel.addPerson(new Student(1,"thang",18));
-        personModel.addPerson(new Student(2,"huyen",18));
+        personModel.addPerson(new Student(2,"thang2",18));
         System.out.println("Student List");
         personModel.showPerson();
         PersonModel<Employee> personModel1 = new PersonModel<>();
         personModel1.addPerson(new Employee(1,"thang",20000));
-        personModel1.addPerson(new Employee(2,"huyen",3000000));
+        personModel1.addPerson(new Employee(2,"thang2",3000000));
         System.out.println("Employee List");
         personModel1.showPerson();
 
